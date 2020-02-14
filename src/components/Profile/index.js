69e61915d8e39b62c6cd78c1046497648427b03f
David <david_borelli@live.com>
2020-02-13 22:31:03 -0300

    Testing GraphQL Playground

---
+++ src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>David Borelli</h1>
+    <h2>Programador Frontend</h2>
+    <p>
+      Mebro da empresa Foo, escrevo sobre programação, em específico sobre
+      Frontend
+    </p>
+  </div>
+)
+
+export default Profile
