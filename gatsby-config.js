72837af52e44b3ebf7c2c580b693401384325d2a
David <david_borelli@live.com>
2020-02-13 21:06:31 -0300

    First commit and cleanup project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `David Borelli`,
+    description: `A blog abaut development and other cool stuff`,
+    author: `@davidBorelli`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
