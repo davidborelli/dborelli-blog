5eda33dc7c31c6f558ab0096ca3664f825783ccc
David <david_borelli@live.com>
2020-02-14 21:37:25 -0300

    Finished Sidebar

---
+++ src/components/Sidebar/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const SidebarWrapper = styled.aside`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  border-right: 1px solid #38444d;
+  background: #192734;
+  height: 100vh;
+  position: fixed;
+  padding: 2rem;
+  width: 16rem;
+`
