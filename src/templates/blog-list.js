462767cdb5b8b578100e679ad928badc8ebd6508
David <david_borelli@live.com>
2020-02-16 22:05:44 -0300

    Creating pagination component

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -5,8 +5,17 @@ import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 
+import Pagination from "../components/Pagination"
+
 const BlogList = props => {
   const postList = props.data.allMarkdownRemark.edges
+
+  const { currentPage, numPages } = props.pageContext
+
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? "/" : `/page/${currentPage - 1}`
+  const nextPage = `/page/${currentPage + 1}`
 
   return (
     <Layout>
@@ -36,6 +45,15 @@ const BlogList = props => {
           )
         }
       )}
+
+      <Pagination
+        isFirst={isFirst}
+        isLast={isLast}
+        currentPage={currentPage}
+        numPages={numPages}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
     </Layout>
   )
 }
