8c4f25539d640f9764110bb4f71ea394f7e50c33
David <david_borelli@live.com>
2020-02-22 17:51:51 -0300

    Responsive Theme

--- src/components/Pagination/styles.js
+++ src/components/Pagination/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import media from "styled-media-query"
 
 export const PaginationWrapper = styled.section`
   display: flex;
@@ -7,6 +8,11 @@ export const PaginationWrapper = styled.section`
   padding: 1.5rem 3rem;
   border-top: 1px solid var(--borders);
   color: var(--texts);
+
+  ${media.lessThan("large")`
+    font-size: .8rem;
+    padding: 1rem;
+  `}
 
   a {
     color: var(--texts);
